0de0b8239dd261d5fb12f93169d35b0da379e5c6
ninetyanim <ninetyanim@gmail.com>
2026-07-18 21:59:21 +0800

    修改無條件存檔的規則

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'memoire-v39'; // 每次升級主程式版本時務必同步改這裡，否則瀏覽器不會抓新版
+const CACHE_NAME = 'memoire-v40'; // 每次升級主程式版本時務必同步改這裡，否則瀏覽器不會抓新版
 const ASSETS = [
   '/Memoire/',
   '/Memoire/index.html',
